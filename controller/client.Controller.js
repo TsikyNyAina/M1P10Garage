09d4b74e1a37a35109525fca31e6fed83cc62d5a
TsikyNyAina <tramasimiarantsoa@gmail.com>
2023-01-18 00:11:56 +0300

    controller
    controller rest client

--- controller/client.Controller.js
+++ controller/client.Controller.js
@@ -1,35 +1,30 @@
-const express = require('express');
-const router = express.Router();
+const express = require("express");
 const Client = require('../model/clients');
 
-router.post('/', async (req, res) => {
-    try {
-        const client = await Client.create(req.body);
-        res.status(201).json({ message: 'User created successfully', client });
-    } catch (error) {
-        if (error.code === 11000) { // Mongoose specific duplicate key error code
-            return res.status(409).json({ message: 'Email already exists' });
+class ClientController {
+    rest(app) {
+      const router = express.Router();
+      router.get('/', async (req, res) => {
+        try {
+          const listeClient = await Client.find();
+          res.status(201).json({ message: 'ok', listeClient });
+        } catch (error) {
+          res.status(500).json({ message: error.message });
         }
-        res.status(500).json({ message: error.message });
-    }
-});
+      });
 
-router.get('/', async (req, res) => {
-    try {
-        const listeClient = await Client.find();
-        res.status(201).json({ message: 'User created successfully', listeClient });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+      router.post('/:email/:mdp', async (req, res) => {
+        try {
+            const client = await Client.findOne({ email: req.params.email, mdp: req.params.mdp });
+            res.status(200).json({ client });
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    });
+    
+  
+      app.use('/client', router);
     }
-});
+  }
 
-router.post('/:email/:mdp', async (req, res) => {
-    try {
-        const client = await Client.findOne({ email: req.params.email, mdp: req.params.mdp });
-        res.status(200).json({ client });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
-
-module.exports = router;
+module.exports = new ClientController();
