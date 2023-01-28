3902256e1a3ca5faba6c13f975cbce57eb87f9f5
tinaandriamarosoa <tina.andriamarosoa@solar-transit.net>
2023-01-28 20:13:38 +0400

    adding cors

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@
 import * as controller from "./controller";
 import * as cast from "./strict/cast";
 import * as rest from "./strict/rest";
-
+import cors from "cors"
 
 import dotenv from "dotenv";
 import express from "express";  
@@ -24,7 +24,7 @@ app.use(
     swaggerUi.serve, 
     swaggerUi.setup(swaggerJson,{ explorer: true })
 );
-
+app.use(cors())
 
 for(let c of Object.values(controller) )new c().rest(app)
 const port=process.env.port||3000
