59f5de404e3f3caa7327d3271f0a53b23d5e6796
tinaandriamarosoa <tina.andriamarosoa@solar-transit.net>
2023-01-28 19:58:40 +0400

    commit

--- datasource/index.ts
+++ datasource/index.ts
@@ -19,15 +19,6 @@ export const connect:()=>Promise<MongoClientWithDatabase>=()=>new MongoClient(ur
     return client as any
 }) 
 
-
-// <<<<<<< HEAD
-
-// =======
-// let uri=`mongodb+srv://${username}:${password}@${host}/?retryWrites=true&w=majority`
-// // uri='mongodb+srv://vaika:vaika@vaika.pzzbpw6.mongodb.net/?retryWrites=true&w=majority';
-// // console.log(uri);
-// uri='mongodb://127.0.0.1:27017/vaika';
-// >>>>>>> 6f2c72d322b35a6a8d7b7f98fb0bf376e8acd12e
-
+ 
 
 export default connect
